cd3b3b6a45068de7630eaa7cc0b988ecf36cbf92
Vinicius <vinilcoelho_25@hotmail.com>
2023-03-14 17:31:12 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portifólio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
